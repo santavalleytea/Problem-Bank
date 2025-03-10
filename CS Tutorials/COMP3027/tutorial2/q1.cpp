7c2fbe0d738859c47b1640ed1672f846c175de5a
Takaya <takayaueno0712@gmail.com>
2025-03-10 21:08:38 +1100

    Added LeetCode solution

--- CS Tutorials/COMP3027/tutorial2/q1.cpp
+++ CS Tutorials/COMP3027/tutorial2/q1.cpp
@@ -3,24 +3,28 @@
 #include <algorithm>
 #include <utility>
 
-struct Job {
+struct Job
+{
     int weight;
     int time;
 };
 
-bool compareJobs(const Job &a, const Job &b) {
+bool compareJobs(const Job &a, const Job &b)
+{
     return (static_cast<double>(a.weight) / a.time) > (static_cast<double>(b.weight) / b.time);
 }
 
 // Compute optimal schedule
-std::pair<long long, std::vector<Job>> scheduleJobs(std::vector<Job> &jobs) {
+std::pair<long long, std::vector<Job>> scheduleJobs(std::vector<Job> &jobs)
+{
     // Sort jobs based on weight to time ratio in descending order
     std::sort(jobs.begin(), jobs.end(), compareJobs);
 
     long long completionTime = 0;
     long long totalWeightedCompletionTime = 0;
 
-    for (std::size_t i = 0; i < jobs.size(); ++i) {
+    for (std::size_t i = 0; i < jobs.size(); ++i)
+    {
         completionTime += jobs[i].time;
         totalWeightedCompletionTime += static_cast<long long>(jobs[i].weight) * completionTime;
     }
@@ -29,7 +33,8 @@ std::pair<long long, std::vector<Job>> scheduleJobs(std::vector<Job> &jobs) {
 }
 
 // Test
-int main() {
+int main()
+{
     // {weight, processing_time}
     std::vector<Job> jobs;
     jobs.push_back({3, 5});
@@ -43,12 +48,11 @@ int main() {
 
     std::cout << "Optimal Weighted Sum of Completion Times: " << optimalWeightedSum << std::endl;
     std::cout << "Optimal Job Order (weight, time):" << std::endl;
-    for (std::size_t i = 0; i < sortedJobs.size(); i++) {
+    for (std::size_t i = 0; i < sortedJobs.size(); i++)
+    {
         std::cout << "(" << sortedJobs[i].weight << ", " << sortedJobs[i].time << ") ";
     }
     std::cout << std::endl;
 
     return 0;
 }
-
-
---
+++ LeetCode/Arrays & Hashing/contains_duplicate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+    public:
+        bool containsDuplicate(vector<int>& nums) {
+            // Unordered set with unique elements
+            std::unordered_set<int> vals;
+    
+            for (int i = 0; i < nums.size(); ++i) {
+                if (vals.find(nums[i]) != vals.end()) {
+                    return true;
+                } else {
+                    vals.insert(nums[i]);
+                }
+            }
+    
+            return false;
+        }
+    };
