ab3fa63dec8d745975e1866e41e5e6bca614731a
Takaya <takayaueno0712@gmail.com>
2025-03-12 19:36:58 +1100

    Added two sum

---
+++ LeetCode/Arrays & Hashing/twosum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+    public:
+        std::vector<int> twoSum(std::vector<int>& nums, int target) {
+            std::unordered_map<int, int>map;
+    
+            for (int i = 0; i < nums.size(); ++i) {
+                int delta = target - nums[i];
+    
+                if (map.find(delta) != map.end()) {
+                    return {map[delta], i};
+                }
+    
+                map[nums[i]] = i;
+            }
+    
+            return {};
+        }
+    };
+    
+    /**
+    Naive Solution:
+    - Iterate through array
+    - For each element, make another iteration and check if it adds up to the target value
+    - O(n^2) worst case
+    
+    Optimal:
+    - Have a hash map with int-int pairs
+    - Check the complement and see if it is in the hash
+    - if it exists, return the complement and i
+    - else, add it to map
+    **/ 
